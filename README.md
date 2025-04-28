488ad1703f9aa93df9b666edc8451b1eed902889
Maratib Ali Khan <maratib@gmail.com>
2025-04-28 16:53:12 +0500

    initial commit

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # WP-Theme
-#### Wordpress starter theme with blade templating
+#### Wordpress starter theme with tailwindcss, FSE, Block themes
 
 ### Installation 
 ```bash 
-composer create-project webpup/wp-theme my-theme
+git clone https://github.com/webpup/wp-theme.git
 ```
-
 
 ## Docs
 [webpup.github.io/wp-theme](https://webpup.github.io/wp-theme)
---
+++ patterns/hero.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: Call to Action
+ * Slug: wp-theme/hero
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+    <!-- wp:heading -->
+    <h2>Ready to get started?</h2>
+    <!-- /wp:heading -->
+    
+    <!-- wp:buttons -->
+    <div class="wp-block-buttons">
+        <!-- wp:button -->
+        <div class="wp-block-button">
+            <a class="wp-block-button__link">Contact Us</a>
+        </div>
+        <!-- /wp:button -->
+    </div>
+    <!-- /wp:buttons -->
+</div>
+<!-- /wp:group -->
